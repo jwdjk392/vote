ca503cd9d9eddddca3eecda9cd5958ca7d059dc9
jwdjk392 <jeongwan.bae@outlook.com>
2026-02-24 23:26:00 +0900

    1.4.1: dependency fix

--- modules/menudesign.js
+++ modules/menudesign.js
@@ -28,13 +28,13 @@ function menuMaker(isDev) {
             {
                 label: "제대로 작동하지 않을때 연락해야 할 곳",
                 click: function () {
-                    require('electron').dialog.showMessageBoxSync(null, {title: "개발자 연락처", message: "이메일 jayden.bae@outlook.kr\n반,번호,이름: 7반 5번 배정완\n\n연락을 하실때 오작동 영상, 재현방법등을 함께 보내주시면 감사하겠습니다.", type: "info"})
+                    require('electron').dialog.showMessageBoxSync(null, {title: "개발자 연락처", message: "이메일 jayden.bae@outlook.kr\n\n연락을 하실때 오작동 영상, 재현방법등을 함께 보내주시면 감사하겠습니다.", type: "info"})
                 }
             },
             {
                 label: "정보",
                 click: function () {
-                    require('electron').dialog.showMessageBoxSync(null, {message: "배정완이 (20705) 만든 투표 추첨 프로그램\n버전: " + require('electron').app.getVersion() + "\n\n사용해주셔서 감사합니다.\n\nAll Audio from Pixabay", title: "정보", type: 'info'})
+                    require('electron').dialog.showMessageBoxSync(null, {message: "JWDJK392가 만든 투표 추첨 프로그램\n버전: " + require('electron').app.getVersion() + "\n\n사용해주셔서 감사합니다.\n\nAll Audio from Pixabay", title: "정보", type: 'info'})
                 }
             }
         ]}
